d8ee94024ecc53570469691b95b1732f0510055b
JoshOrais <michaeljoshuaorais@gmail.com>
2019-11-27 00:04:15 +0800

    Mapping class updated

--- Mapping.java
+++ Mapping.java
@@ -1,9 +1,24 @@
 public class Mapping{
-    public void writeTree(){
+    public void writeTree(Node root, long num){
+        if(root.getLeft() == null && root.getRight() == null){
+            root.getKey();
+            //get binary representation of long
+            //write key and long
+        }
 
+        Node(root.getLeft(), num<<);
+        Node(root.getRight(), num>>|1);        
     }
 
-    public void writeCompressed(){
-        
+    public void writeCompressed(int[][] pixels){
+        int row = pixels.length();
+        int column = pixels[0].length();
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                //search key at tree file
+                //write binary representation
+            }
+        }
     }
 }

e98b00696a5bdc57eaac8df060dbfd9bf1f34394
JoshOrais <michaeljoshuaorais@gmail.com>
2019-12-03 23:03:34 +0800

    Update WriteCompressed.java

--- WriteCompressed.java
+++ WriteCompressed.java
@@ -6,5 +6,14 @@ public class WriteCompressed{
 
         int row = pixelArr.length;
         int column = pixelArr[0].length;
+
+        //sort uniqueArr through key 
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                //search pixelArr[i][j] at sorted uniqueArr
+                //write matching uniqueArr.getBitString();
+            }
+        }
     }
 }

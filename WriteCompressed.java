2b45b342dcdd9164d1db5a6000ceeab09d8f5bca
JoshOrais <michaeljoshuaorais@gmail.com>
2019-12-03 20:17:41 +0800

    Update WriteCompressed.java

--- WriteCompressed.java
+++ WriteCompressed.java
@@ -1,21 +1,10 @@
 public class WriteCompressed{
-    /*private Mapping map = new Mapping();
+    private Mapping map = new Mapping();
 
     public WriteCompressed(Node[] uniqueArr, Node root, int[][] pixelArr){
         map.createPath(root, 0);
 
         int row = pixelArr.length;
         int column = pixelArr[0].length;
-
-        //sort uniqueArr through key 
-
-        for(int i=0; i<row; i++){
-            for(int j=0; j<column; j++){
-                //search pixelArr[i][j] at sorted uniqueArr
-                //write matching uniqueArr.getBitString();
-            }
-        }
     }
-
-    private*/
 }

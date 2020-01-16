b975623c210227719e4f84097702094ee6e5884a
JoshOrais <michaeljoshuaorais@gmail.com>
2020-01-16 19:07:41 +0800

    recreate image

--- RecreateImage.java
+++ RecreateImage.java
@@ -16,26 +16,51 @@ public class RecreateImage{
 
         //convert byte array to string
         
-        Node node = root;
-        String str = "";
-        int strTracker = 0;
+        // Node node = root;
+        // String str = "";
+        // int strTracker = 0;
 
-        for(int i=2; i<(fileContent.length); i++){
-            //System.out.println("Converting \t" + i);
-            str = str + (String.format("%8s", Integer.toBinaryString((fileContent[i]) & 0xFF)).replace(' ', '0'));
-        }
-        System.out.println("Done Creating String");
+        // for(int i=2; i<(fileContent.length); i++){
+        //     //System.out.println("Converting \t" + i);
+        //     str = str + (String.format("%8s", Integer.toBinaryString((fileContent[i]) & 0xFF)).replace(' ', '0'));
+        // }
+        // System.out.println("Done Creating String");
+
+        // for(int i=0; i<row; i++){
+        //     for(int j=0; j<col; j++){
+        //         while((node.getLeft() != null) && (node.getRight() != null)){
+        //             if(str.charAt(strTracker) == '1'){
+        //                 node = node.getLeft();
+        //             }
+        //             else if(str.charAt(strTracker) == '0'){
+        //                 node = node.getRight();
+        //             }
+        //             strTracker++;
+        //         }
+        //         image.setRGB(i, j, node.getKey());
+        //     }
+        // }
+
+        Node node = root;
+        int strTracker = 0;
+        int length = 8;
 
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 while((node.getLeft() != null) && (node.getRight() != null)){
-                    if(str.charAt(strTracker) == '1'){
-                        node = node.getLeft();
+                    if(length < 0){
+                        if(((fileContent[strTracker] >> (length-1)) & 1) == 1){
+                            node = node.getLeft();
+                        }
+                        else if(((fileContent[strTracker] >> (length-1)) & 1) == 0){
+                            node = node.getRight();
+                        }
+                        length--;
                     }
-                    else if(str.charAt(strTracker) == '0'){
-                        node = node.getRight();
+                    else if(length == 0){
+                        strTracker++;
+                        length = 8;
                     }
-                    strTracker++;
                 }
                 image.setRGB(i, j, node.getKey());
             }
